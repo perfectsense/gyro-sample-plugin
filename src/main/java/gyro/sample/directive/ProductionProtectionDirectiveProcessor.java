bff40089cb7e43fdf6d6c75a67341377c5b15b7d
Hao Sun <PSDhao@users.noreply.github.com>
2019-10-03 18:13:04 -0400

    Change production directive to FileScope based

--- src/main/java/gyro/sample/directive/ProductionProtectionDirectiveProcessor.java
+++ src/main/java/gyro/sample/directive/ProductionProtectionDirectiveProcessor.java
@@ -4,15 +4,15 @@ import gyro.core.GyroCore;
 import gyro.core.GyroUI;
 import gyro.core.Type;
 import gyro.core.directive.DirectiveProcessor;
-import gyro.core.scope.RootScope;
+import gyro.core.scope.FileScope;
 import gyro.lang.ast.block.DirectiveNode;
 import gyro.sample.ProdutionGyroUi;
 
 @Type("production")
-public class ProductionProtectionDirectiveProcessor extends DirectiveProcessor<RootScope> {
+public class ProductionProtectionDirectiveProcessor extends DirectiveProcessor<FileScope> {
 
     @Override
-    public void process(RootScope rootScope, DirectiveNode directiveNode) throws Exception {
+    public void process(FileScope fileScope, DirectiveNode directiveNode) throws Exception {
         GyroUI ui = GyroCore.popUi();
         if (!(ui instanceof ProdutionGyroUi)) {
             ui = new ProdutionGyroUi(ui);
